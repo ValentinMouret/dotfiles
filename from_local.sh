2e772148c8610ef1155a66b7e50bf0fd0e3e9843
Valentin Mouret <valentin.mou@gmail.com>
2023-06-13 09:01:35 +0200

    Add copy of the zsh config from local

--- from_local.sh
+++ from_local.sh
@@ -2,18 +2,31 @@
 
 # Copies the local emacs and zsh setup to the local dotfiles repository.
 
+set -euo pipefail
+
 DEV_PATH=${DEV_PATH:-$HOME/Developer/valentinmouret}
 DOTFILES_PATH=$DEV_PATH/dotfiles
 
-if ! [[ -d $DOTFILES_PATH ]]; then
-  echo "$DOTFILES_PATH does not exist"
-  exit 1
-fi
+function copy::emacs () {
+  if ! [[ -d $DOTFILES_PATH ]]; then
+    echo "$DOTFILES_PATH does not exist"
+    exit 1
+  fi
 
-EMACS_PATH=$HOME/.emacs.d
+  EMACS_PATH=$HOME/.emacs.d
 
-cp $EMACS_PATH/init.el $DOTFILES_PATH/.emacs.d/init.el
-cp $EMACS_PATH/early-init.el $DOTFILES_PATH/.emacs.d/early-init.el
+  cp $EMACS_PATH/init.el $DOTFILES_PATH/.emacs.d/init.el
+  cp $EMACS_PATH/early-init.el $DOTFILES_PATH/.emacs.d/early-init.el
 
-rm $DOTFILES_PATH/.emacs.d/elisp/*
-cp $EMACS_PATH/elisp/*.el $DOTFILES_PATH/.emacs.d/elisp/
+  rm $DOTFILES_PATH/.emacs.d/elisp/*
+  cp $EMACS_PATH/elisp/*.el $DOTFILES_PATH/.emacs.d/elisp/
+}
+
+function copy::zsh () {
+  cp $HOME/.zshrc $DOTFILES_PATH/.zshrc
+  zsh_modules_path=$HOME/.zsh
+  
+  cp $HOME/.zsh/* $DOTFILES_PATH/.zsh
+}
+
+copy::emacs && copy::zsh
