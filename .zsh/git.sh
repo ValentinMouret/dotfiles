aeac4d867dcd15fa81bebfecb95d43efb417f96e
Valentin Mouret <valentin.mou@gmail.com>
2023-06-15 11:19:16 +0200

    Add some git aliases

--- .zsh/git.sh
+++ .zsh/git.sh
@@ -1,10 +1,13 @@
 #!/bin/zsh
 
+alias ga="git add"
 alias gs="git status"
 alias gc="git commit"
 alias gca="git commit --amend"
 alias gcp="git cherry-pick"
 alias gl="git prettylog"
+alias gd="git diff"
+alias gp="git push"
 
 git config --global alias.prettylog "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(r) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 git config --global alias.root "rev-parse --show-toplevel"
